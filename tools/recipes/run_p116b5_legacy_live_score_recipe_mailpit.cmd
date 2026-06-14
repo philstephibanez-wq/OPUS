e7597ea4091bb8d54506f7ec6e5d9069df36f236
Steve Ibanez <philstephibanez@gmail.com>
2026-06-14 02:30:46 +0200

    P116B5 keep legacy runner alias to canonical live ScoreTemplate runner

--- tools/recipes/run_p116b5_legacy_live_score_recipe_mailpit.cmd
+++ tools/recipes/run_p116b5_legacy_live_score_recipe_mailpit.cmd
@@ -1,28 +1,7 @@
 @echo off
-setlocal
+setlocal EnableExtensions
 cd /d "%~dp0\..\.."
 
-if not defined OPUS_P112Q3B2_REPORT_EMAIL_TO (
-  set /p OPUS_P112Q3B2_REPORT_EMAIL_TO=Email destinataire du rapport P116B5 : 
-)
-
-if not defined OPUS_P112Q3B2_REPORT_EMAIL_FROM set OPUS_P112Q3B2_REPORT_EMAIL_FROM=opus-recipes@localhost
-if not defined OPUS_P112Q3B2_SMTP_HOST set OPUS_P112Q3B2_SMTP_HOST=127.0.0.1
-if not defined OPUS_P112Q3B2_SMTP_PORT set OPUS_P112Q3B2_SMTP_PORT=1025
-if not defined OPUS_P116B5_REPORT_PORT set OPUS_P116B5_REPORT_PORT=8795
-
-php tools\recipes\p116b5_legacy_live_score_recipe.php
-set EXITCODE=%ERRORLEVEL%
-
-if exist "var\reports\p116b5\p116b5_legacy_live_score_recipe.html" (
-  echo.
-  echo Ouverture du rapport ScoreTemplate local...
-  start "Opus P116B5 Score Report Server" cmd /k php -S 127.0.0.1:%OPUS_P116B5_REPORT_PORT% -t "var\reports\p116b5"
-  timeout /t 2 /nobreak >nul
-  start "" "http://127.0.0.1:%OPUS_P116B5_REPORT_PORT%/p116b5_legacy_live_score_recipe.html"
-)
-
-echo.
-echo ExitCode=%EXITCODE%
-pause
-exit /b %EXITCODE%
+echo P116B5_LEGACY_ALIAS: use run_p116b5_live_score_recipe_mailpit.cmd
+call tools\recipes\run_p116b5_live_score_recipe_mailpit.cmd
+exit /b %ERRORLEVEL%
