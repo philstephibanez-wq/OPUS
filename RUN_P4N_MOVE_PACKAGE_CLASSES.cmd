62d9be5b94e9d988353754170039c96f4d44940f
Steve Ibanez <philstephibanez@gmail.com>
2026-06-23 19:49:01 +0200

    P4N_MARK_PACKAGE_MOVE_RUNNER_OBSOLETE

--- RUN_P4N_MOVE_PACKAGE_CLASSES.cmd
+++ RUN_P4N_MOVE_PACKAGE_CLASSES.cmd
@@ -1,29 +1,7 @@
 @echo off
 setlocal
 cd /d "%~dp0"
-echo == P4N_MOVE_PACKAGE_CLASSES ==
-
-python tools\migrations\apply_p4n_move_package_classes.py
-if errorlevel 1 goto fail
-
-php -l Opus\Package\Package.php
-if errorlevel 1 goto fail
-php -l Opus\Package\PackageRepository.php
-if errorlevel 1 goto fail
-php -l Opus\Kernel.php
-if errorlevel 1 goto fail
-php -l Opus\Router.php
-if errorlevel 1 goto fail
-
-composer dump-autoload
-if errorlevel 1 goto fail
-
-php -r "require 'vendor/autoload.php'; foreach (['Opus\\Package\\Package','Opus\\Package\\PackageRepository'] as $class) { if (!class_exists($class)) { fwrite(STDERR, 'AUTOLOAD_MISSING=' . $class . PHP_EOL); exit(1); } } foreach (['Opus\\Package','Opus\\PackageRepository'] as $class) { if (class_exists($class)) { fwrite(STDERR, 'LEGACY_CLASS_STILL_AUTOLOADS=' . $class . PHP_EOL); exit(1); } } echo 'P4N_PACKAGE_AUTOLOAD_OK', PHP_EOL;"
-if errorlevel 1 goto fail
-
-echo P4N_MOVE_PACKAGE_CLASSES_OK
-exit /b 0
-
-:fail
-echo P4N_MOVE_PACKAGE_CLASSES_FAILED
+echo == P4N_MOVE_PACKAGE_CLASSES_OBSOLETE ==
+echo P4N is obsolete: Package is reserved for delivery/install packaging.
+echo Use RUN_P4N2_RENAME_RUNTIME_PACKAGE_TO_APPLICATION.cmd instead.
 exit /b 1
