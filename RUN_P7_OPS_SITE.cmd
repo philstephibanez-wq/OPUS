a303c18eeb6ce59a0f52ca8409b83594530c2006
Steve Ibanez <philstephibanez@gmail.com>
2026-06-30 20:44:31 +0200

    Fix P7 OPS site launcher path

--- RUN_P7_OPS_SITE.cmd
+++ RUN_P7_OPS_SITE.cmd
@@ -1,4 +1,4 @@
 @echo off
 setlocal EnableExtensions
-cd /d "%%~dp0"
-php -S 127.0.0.1:8078 -t "%%~dp0sites\opus-p7-ops\public" "%%~dp0sites\opus-p7-ops\public\router.php"
+cd /d "%~dp0"
+php -S 127.0.0.1:8078 -t "%~dp0sites\opus-p7-ops\public" "%~dp0sites\opus-p7-ops\public\router.php"
