3da8c09a65a9fe247c20b68a5ca16d1b8dd48a5a
Steve Ibanez <philstephibanez@gmail.com>
2026-07-17 10:21:06 +0200

    Add server-side OWASYS navigation definition

---
+++ sites/owasys/application/default/navigation/menu.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+return [
+    ['path' => '/', 'label_key' => 'menu.home', 'order' => 10],
+    ['path' => '/applications', 'label_key' => 'menu.applications', 'order' => 20],
+    ['path' => '/structure', 'label_key' => 'menu.structure', 'order' => 30],
+    ['path' => '/source', 'label_key' => 'menu.source', 'order' => 40],
+    ['path' => '/data', 'label_key' => 'menu.data', 'order' => 50],
+    ['path' => '/workflows', 'label_key' => 'menu.workflows', 'order' => 60],
+    ['path' => '/security', 'label_key' => 'menu.security', 'order' => 70],
+    ['path' => '/build', 'label_key' => 'menu.build', 'order' => 80],
+];
