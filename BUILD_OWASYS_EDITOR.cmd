01d850f8815c2807b36331cc5f6709658fb9d730
Steve Ibanez <philstephibanez@gmail.com>
2026-07-17 02:27:39 +0200

    Clean OWASYS frontend build dependencies after bundling

--- BUILD_OWASYS_EDITOR.cmd
+++ BUILD_OWASYS_EDITOR.cmd
@@ -3,9 +3,16 @@ setlocal
 cd /d "%~dp0"
 where npm >nul 2>&1 || exit /b 1
 call npm install
-if errorlevel 1 exit /b 1
+if errorlevel 1 goto :fail
 call npm run build:owasys-editor
-if errorlevel 1 exit /b 1
+if errorlevel 1 goto :fail
 php tools\smoke_owasys_codemirror.php
-if errorlevel 1 exit /b 1
+if errorlevel 1 goto :fail
+rmdir /s /q node_modules
 endlocal
+exit /b 0
+
+:fail
+set "OWASYS_EDITOR_BUILD_ERROR=%ERRORLEVEL%"
+if exist node_modules rmdir /s /q node_modules
+endlocal & exit /b %OWASYS_EDITOR_BUILD_ERROR%
