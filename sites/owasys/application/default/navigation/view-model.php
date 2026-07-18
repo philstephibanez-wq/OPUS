b876f7c7396234b11ac89fd82d4b2129297c4b8c
Steve Ibanez <philstephibanez@gmail.com>
2026-07-18 11:56:51 +0200

    Project canonical GET links for OWASYS navigation

--- sites/owasys/application/default/navigation/view-model.php
+++ sites/owasys/application/default/navigation/view-model.php
@@ -6,7 +6,7 @@ use Opus\Fsm\FsmProcessor;
 /**
  * Build the navigation fragment of the OWASYS ScoreTemplate ViewModel.
  *
- * @return array{items:list<array<string,mixed>>,action:string,current_state:string,aria_label:string}
+ * @return array{items:list<array<string,mixed>>,current_state:string,aria_label:string}
  */
 return static function (
     FsmProcessor $fsm,
@@ -15,22 +15,24 @@ return static function (
     string $profile,
     array $presentation,
     array $acl,
-    string $actionUrl,
+    string $applicationBaseUrl,
     callable $translate
 ): array {
     $project = require __DIR__ . '/project.php';
     $items = $project($fsm, $currentState, $runtimeContext, $profile, $presentation, $acl);
 
+    $base = rtrim($applicationBaseUrl, '/');
     foreach ($items as &$item) {
         $labelKey = (string) ($item['label_key'] ?? '');
+        $targetRoute = '/' . ltrim((string) ($item['target_route'] ?? ''), '/');
         $item['label'] = $translate($labelKey);
         $item['current'] = ((string) ($item['target_state'] ?? '')) === $currentState;
+        $item['href'] = $base . ($targetRoute === '/' ? '/' : $targetRoute);
     }
     unset($item);
 
     return [
         'items' => $items,
-        'action' => $actionUrl,
         'current_state' => $currentState,
         'aria_label' => $translate('navigation.aria_label'),
     ];
