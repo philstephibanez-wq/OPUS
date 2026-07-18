a4a2dec92b640a52ea21949b886373f2954d7b72
Steve Ibanez <philstephibanez@gmail.com>
2026-07-18 12:03:51 +0200

    Delete obsolete OWASYS legacy application entry

--- sites/owasys/application/application.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-
-http_response_code(410);
-header('Content-Type: text/plain; charset=UTF-8');
-header('Cache-Control: no-store');
-
-echo "OWASYS_LEGACY_APPLICATION_REMOVED\n";
