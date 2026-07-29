f427f67f18fcb4d67a69d1987ad031e2cfc7c7ec
Steve Ibanez <philstephibanez@gmail.com>
2026-07-29 12:50:58 +0200

    cleanup

--- sites/owasys-front/application/shared/RuntimeInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-interface OwasysProcessRuntimeInterface
-{
-    public function mode(): string;
-    public function run(): void;
-    public function fail(Throwable $error, string $traceId): void;
-}
